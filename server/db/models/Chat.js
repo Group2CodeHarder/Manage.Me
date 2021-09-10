f820fb36cf1e64e566c42e49d4eeb68132c1ef48
SantaRedux <72110198+SantaRedux@users.noreply.github.com>
2021-09-10 13:50:06 -0500

    Update Chat.js

--- server/db/models/Chat.js
+++ server/db/models/Chat.js
@@ -1,1 +1,13 @@
+const db = require('../db');
 
+const Chat = db.define('chat', {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    },
+  },
+});
+
+module.exports = Chat;
