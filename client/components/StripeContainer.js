f54632b8675e64123320ccb801e191d4f29d431d
Zoe Liu <zliu27@u.rochester.edu>
2021-09-18 12:46:53 -0400

    stipe progress

---
+++ client/components/StripeContainer.js
@@ -0,0 +1,17 @@
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+import React from "react";
+import PaymentForm from "./PaymentForm";
+
+const PUBLIC_KEY =
+  "pk_test_51Jb57wGAp8Evh66uodNbWtevgxEhjTOLSIVfo8qX3DY9Awhlnzkk177b1JkRBe12gsZgvicUVHZk9UNDzgQY6I0400j638RUeV";
+
+const stripeTestPromise = loadStripe(PUBLIC_KEY);
+
+export default function StripeContainer() {
+  return (
+    <Elements stripe={stripeTestPromise}>
+      <PaymentForm />
+    </Elements>
+  );
+}
