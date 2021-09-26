c08160117bcec1f34566a0babf1694cf8d544404
Sebastien Noel <snoel45@gmail.com>
2021-09-26 17:38:17 -0400

    cleanup

--- server/api/calendar.js
+++ server/api/calendar.js
@@ -3,7 +3,42 @@ const { oauth2Client, scopes, google } = require("../auth/googleClient");
 
 const calendar = google.calendar("v3");
 
-router.post("/", (req, res) => {});
+router.post("/", (req, res) => {
+  try {
+    const event = req.body;
+    calendar.freebusy.query(
+      {
+        resource: {
+          timeMin: event.start.dateTime,
+          timeMax: event.end.dateTime,
+          timeZone: "America/New_York",
+          items: [{ id: "primary" }],
+        },
+      },
+      (err, res) => {
+        if (err) return console.error("free busy error", err);
+        const eventsArr = res.data.calendars.primary.busy;
+        //code to create a calendar event
+        if (eventsArr.length === 0)
+          return calendar.events.insert(
+            {
+              calendarId: "primary",
+              resource: event,
+            },
+            (err) => {
+              if (err)
+                return console.error("calendar event creation error", err);
+              return console.log("calendar event created");
+            }
+          );
+        console.log("sorry i`m busy");
+      }
+    );
+    res.status(201);
+  } catch (err) {
+    console.log(err);
+  }
+});
 
 router.get("/", async (req, res) => {
   const events = await calendar.events.list({
