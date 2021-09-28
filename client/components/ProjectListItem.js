781cd078a86b1a61c76b37cc9055ff740a6f3938
Zoe Liu <zliu27@u.rochester.edu>
2021-09-28 18:53:25 -0400

    finance dashboard functionality complete

--- client/components/ProjectListItem.js
+++ client/components/ProjectListItem.js
@@ -1,22 +1,21 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
-const ProjectListItem = props => {
+const ProjectListItem = (props) => {
   const { project } = props;
- 
-  const deadline = `${project.deadlineMonth} ${project.deadlineDate}, ${project.deadlineYear}`;  
-  
+
+  const deadline = `${project.deadlineMonth} ${project.deadlineDate}, ${project.deadlineYear}`;
+
   return (
-      <Link to= {`/projects/${project.id}`}>
-      <div id= 'project-list-item'>
+    <Link to={`/projects/${project.id}`}>
+      <div id="project-list-item">
         <h4>{project.name}</h4>
         <p>{deadline}</p>
         <p>{project.status}</p>
         <p>{project.clientName}</p>
-        </div>
-      </Link>
-  )
-}
-
+      </div>
+    </Link>
+  );
+};
 
 export default ProjectListItem;
